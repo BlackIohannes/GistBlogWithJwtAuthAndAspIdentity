fa59ef98db4805e6713aac98f6e6c3002f0cac53
johncreator <chukwuchidieberejohn@gmail.com>
2023-09-19 20:51:50 +0100

    added sorting and filtering

--- GistBlog.BLL/Extensions/Middlewares.cs
+++ GistBlog.BLL/Extensions/Middlewares.cs
@@ -6,6 +6,7 @@ using GistBlog.DAL.Repository.Contracts;
 using GistBlog.DAL.Repository.Implementations;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace GistBlog.BLL.Extensions;
 
@@ -20,11 +21,10 @@ public static class Middlewares
         services.AddScoped<EmailSettings>();
         services.Configure<MailjetSettings>(configuration.GetSection("MailjetSettings"));
         services.AddScoped<IMailjetService, MailjetService>();
-        services.AddAuthentication()
-            .AddGoogle(options =>
-            {
-                options.ClientId = "53306076231-4jrvd3b85ue9dk7otluf5hrtq6keu9fv.apps.googleusercontent.com";
-                options.ClientSecret = "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";
-            });
+        services.AddLogging(building =>
+        {
+            building.AddConsole();
+            building.AddDebug();
+        });
     }
 }
